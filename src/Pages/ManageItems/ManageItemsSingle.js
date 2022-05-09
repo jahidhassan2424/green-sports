7b609377075bb913676f4ea4734783fc2d105809
jahidhassan2424 <jahidhassan.programmer@gmail.com>
2022-05-09 14:14:21 +0600

    Manage page table,icon added

--- src/Pages/ManageItems/ManageItemsSingle.js
+++ src/Pages/ManageItems/ManageItemsSingle.js
@@ -1,17 +1,30 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { faCirclePlus, faCoffee, faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
 
 const ManageItemsSingle = ({ p }) => {
-    const { name, quantity, supplier, sold } = p;
+    const navigate = useNavigate();
+    const { name, quantity, supplier, sold, _id } = p;
     let count = 1;
+    const handleAddItem = () => {
+        navigate
+    }
+    const handleRemoveItem = () => {
+
+    }
+
     return (
         <tr>
             <td>{name}</td>
             <td>{quantity}</td>
             <td>{sold}</td>
             <td>{supplier}</td>
-            <td style={{ cursor: "pointer" }} className="tooltip">Hover over me
-                <span class="tooltiptext">Tooltip text</span>A</td>
-            <td style={{ cursor: "pointer" }} >X</td>
+            <td onClick={() => handleAddItem(_id)} style={{ cursor: "pointer", fontSize: "25px" }} className='text-center text-success'><FontAwesomeIcon icon={faCirclePlus} /></td>
+
+            <td onClick={() => handleRemoveItem(_id)} style={{ cursor: "pointer", fontSize: "25px" }} className='text-center text-danger'><FontAwesomeIcon icon={faTrashCan} /></td>
+
 
         </tr>
     );
